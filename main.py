efa244bc49e767234be3e7b3450bc5a8c839d1ca
Sreya Saju <68282466+sreyasaju@users.noreply.github.com>
2025-08-07 03:22:22 +0300

    add mysql connector

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
 import mysql.connector as sqlconnector
+import hashlib
 
 mycon = sqlconnector.connect(host="localhost", user="root", passwd="", database="test")
 if mycon.is_connected():
     print("Sucessfully connected to MySQL database")
 cursor = mycon.cursor()
+
+cursor.execute("""
+    CREATE TABLE IF NOT EXISTS userdata (
+        id INTEGER PRIMARY KEY, 
+        username VARCHAR(255) NOT NULL,
+        password VARCHAR(255) NOT NULL
+    )
+""")
