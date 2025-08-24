f7864cb3a0aa7761625a78f39f2bbdb59dce5aad
Sreya Saju <68282466+sreyasaju@users.noreply.github.com>
2025-08-25 00:33:43 +0300

    add login and signup dialogs

---
+++ ui_signupdialog.py
@@ -0,0 +1,93 @@
+# -*- coding: utf-8 -*-
+
+################################################################################
+## Form generated from reading UI file 'signupdialog.ui'
+##
+## Created by: Qt User Interface Compiler version 6.9.1
+##
+## WARNING! All changes made in this file will be lost when recompiling UI file!
+################################################################################
+
+from PySide6.QtCore import (QCoreApplication, QDate, QDateTime, QLocale,
+    QMetaObject, QObject, QPoint, QRect,
+    QSize, QTime, QUrl, Qt)
+from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
+    QFont, QFontDatabase, QGradient, QIcon,
+    QImage, QKeySequence, QLinearGradient, QPainter,
+    QPalette, QPixmap, QRadialGradient, QTransform)
+from PySide6.QtWidgets import (QAbstractButton, QApplication, QDialog, QDialogButtonBox,
+    QGroupBox, QLabel, QLineEdit, QSizePolicy,
+    QWidget)
+
+class Ui_signupDialog(object):
+    def setupUi(self, signupDialog):
+        if not signupDialog.objectName():
+            signupDialog.setObjectName(u"signupDialog")
+        signupDialog.resize(406, 330)
+        signupDialog.setStyleSheet(u"background-color: #e2fbff;\n"
+"border-radius: 30px")
+        self.groupBox = QGroupBox(signupDialog)
+        self.groupBox.setObjectName(u"groupBox")
+        self.groupBox.setGeometry(QRect(0, 0, 391, 311))
+        self.lineEdit_2 = QLineEdit(self.groupBox)
+        self.lineEdit_2.setObjectName(u"lineEdit_2")
+        self.lineEdit_2.setGeometry(QRect(30, 160, 341, 61))
+        font = QFont()
+        font.setFamilies([u"Baloo Chettan 2"])
+        font.setPointSize(16)
+        self.lineEdit_2.setFont(font)
+        self.lineEdit_2.setStyleSheet(u"background-color: rgb(255, 255, 255);\n"
+"border-radius: 20px;\n"
+"border-width: 10px;\n"
+"border-style: medium;\n"
+"border-color: rgb(0, 207, 230);\n"
+"padding: 12px;")
+        self.lineEdit_3 = QLineEdit(self.groupBox)
+        self.lineEdit_3.setObjectName(u"lineEdit_3")
+        self.lineEdit_3.setGeometry(QRect(30, 90, 341, 61))
+        self.lineEdit_3.setFont(font)
+        self.lineEdit_3.setStyleSheet(u"background-color: rgb(255, 255, 255);\n"
+"border-radius: 20px;\n"
+"border-width: 10px;\n"
+"border-style: medium;\n"
+"border-color: rgb(0, 207, 230);\n"
+"padding: 12px;")
+        self.buttonBox_2 = QDialogButtonBox(self.groupBox)
+        self.buttonBox_2.setObjectName(u"buttonBox_2")
+        self.buttonBox_2.setGeometry(QRect(10, 240, 361, 61))
+        font1 = QFont()
+        font1.setFamilies([u"Baloo Chettan 2"])
+        font1.setPointSize(18)
+        font1.setWeight(QFont.ExtraBold)
+        self.buttonBox_2.setFont(font1)
+        self.buttonBox_2.setStyleSheet(u"background-color: #00dacc;\n"
+"color: rgb(255, 255, 255);\n"
+"border-radius: 15px;\n"
+"padding: 15px;\n"
+"")
+        self.buttonBox_2.setOrientation(Qt.Orientation.Horizontal)
+        self.buttonBox_2.setStandardButtons(QDialogButtonBox.StandardButton.Cancel|QDialogButtonBox.StandardButton.Ok)
+        self.buttonBox_2.setCenterButtons(False)
+        self.label = QLabel(self.groupBox)
+        self.label.setObjectName(u"label")
+        self.label.setGeometry(QRect(40, 50, 81, 21))
+        font2 = QFont()
+        font2.setFamilies([u"Baloo Chettan 2"])
+        font2.setPointSize(20)
+        font2.setBold(True)
+        self.label.setFont(font2)
+        self.label.setStyleSheet(u"color: #00cbbc")
+
+        self.retranslateUi(signupDialog)
+
+        QMetaObject.connectSlotsByName(signupDialog)
+    # setupUi
+
+    def retranslateUi(self, signupDialog):
+        signupDialog.setWindowTitle(QCoreApplication.translate("signupDialog", u"Dialog", None))
+        self.groupBox.setTitle(QCoreApplication.translate("signupDialog", u"GroupBox", None))
+        self.lineEdit_2.setPlaceholderText(QCoreApplication.translate("signupDialog", u"Password", None))
+        self.lineEdit_3.setPlaceholderText(QCoreApplication.translate("signupDialog", u"Username", None))
+        self.label.setText(QCoreApplication.translate("signupDialog", u"Sign Up", None))
+    # retranslateUi
+
