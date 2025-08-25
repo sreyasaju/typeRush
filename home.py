19f7e60d0e9e92a349e0fe8cb94258f3154d75f5
Sreya Saju <68282466+sreyasaju@users.noreply.github.com>
2025-08-26 01:39:07 +0300

    fix navigation between pages and dialogs

--- home.py
+++ home.py
@@ -1,6 +1,8 @@
 # home.py
 from PySide6.QtWidgets import QMainWindow
 from ui.ui_home import Ui_home 
+from settings import SettingsDialog
+from typinggame import TypingGameWindow
 
 class HomeWindow(QMainWindow):
     def __init__(self, parent=None):
@@ -8,12 +10,15 @@ class HomeWindow(QMainWindow):
         self.ui = Ui_home()
         self.ui.setupUi(self)
 
-        self.ui.startbutton.clicked.connect(self.open_typinggame)
+        self.ui.startbutton.clicked.connect(self.open_settings)
         #self.ui.progressbutton.clicked.connect(self.open_progress_window)
         #self.ui.logoutbutton.clicked.connect(self.handle_logout)
 
-    def open_typinggame(self):
-        from typinggame import TypingGameWindow
-        self.typing_game_window = TypingGameWindow()
-        self.typing_game_window.show()
-        self.close()
+    def open_settings(self):
+        settings_dialog = SettingsDialog(self)
+        if settings_dialog.exec() == SettingsDialog.Accepted:
+            # settings dialog closed with Ready
+            duration = settings_dialog.duration
+            self.close()
+            self.typing_game = TypingGameWindow(duration)
+            self.typing_game.show()
