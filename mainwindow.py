24b389a810e59faf1d48879dd85f9f1f7b17c0f8
Sreya Saju <68282466+sreyasaju@users.noreply.github.com>
2025-08-25 12:52:13 +0300

    feat: update mainwindow.py

--- mainwindow.py
+++ mainwindow.py
@@ -3,11 +3,7 @@ import sys
 
 from PySide6.QtWidgets import QApplication, QMainWindow
 
-# Important:
-# You need to run the following command to generate the ui_form.py file
-#     pyside6-uic form.ui -o ui_form.py, or
-#     pyside2-uic form.ui -o ui_form.py
-from ui_mainwindow import Ui_MainWindow
+from ui.ui_mainwindow import Ui_MainWindow
 
 class MainWindow(QMainWindow):
     def __init__(self, parent=None):
@@ -15,9 +11,24 @@ class MainWindow(QMainWindow):
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
 
+        # connect buttons to handlers
+        self.ui.loginbutton.clicked.connect(self.open_login_dialog)
+        self.ui.registerbutton.clicked.connect(self.open_register_dialog)
+
+    def open_login_dialog(self):
+        from login import LoginDialog
+        logindialog = LoginDialog(self)
+        logindialog.exec()    
+
+    def open_register_dialog(self):
+        from register import RegisterDialog
+        registerdialog = RegisterDialog(self)
+        registerdialog.exec()
+
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     widget = MainWindow()
+
     widget.show()
     sys.exit(app.exec())
