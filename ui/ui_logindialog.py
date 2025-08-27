6518116689ba6fd865042933e79a54894ad784a5
Sreya Saju <68282466+sreyasaju@users.noreply.github.com>
2025-08-27 15:14:12 +0300

    fix: update ui py files

--- ui/ui_logindialog.py
+++ ui/ui_logindialog.py
@@ -25,26 +25,56 @@ class Ui_loginDialog(object):
         loginDialog.resize(420, 350)
         loginDialog.setMinimumSize(QSize(420, 350))
         loginDialog.setMaximumSize(QSize(420, 350))
+        palette = QPalette()
+        brush = QBrush(QColor(0, 76, 88, 255))
+        brush.setStyle(Qt.SolidPattern)
+        palette.setBrush(QPalette.Active, QPalette.WindowText, brush)
+        brush1 = QBrush(QColor(226, 251, 255, 255))
+        brush1.setStyle(Qt.SolidPattern)
+        palette.setBrush(QPalette.Active, QPalette.Button, brush1)
+        brush2 = QBrush(QColor(8, 90, 102, 255))
+        brush2.setStyle(Qt.SolidPattern)
+        palette.setBrush(QPalette.Active, QPalette.ButtonText, brush2)
+        palette.setBrush(QPalette.Active, QPalette.Base, brush1)
+        palette.setBrush(QPalette.Active, QPalette.Window, brush1)
+        palette.setBrush(QPalette.Inactive, QPalette.WindowText, brush)
+        palette.setBrush(QPalette.Inactive, QPalette.Button, brush1)
+        palette.setBrush(QPalette.Inactive, QPalette.ButtonText, brush2)
+        palette.setBrush(QPalette.Inactive, QPalette.Base, brush1)
+        palette.setBrush(QPalette.Inactive, QPalette.Window, brush1)
+        palette.setBrush(QPalette.Disabled, QPalette.Button, brush1)
+        palette.setBrush(QPalette.Disabled, QPalette.Base, brush1)
+        palette.setBrush(QPalette.Disabled, QPalette.Window, brush1)
+        loginDialog.setPalette(palette)
+        font = QFont()
+        font.setFamilies([u"Baloo Chettan 2"])
+        loginDialog.setFont(font)
         loginDialog.setStyleSheet(u"background-color: #e2fbff;\n"
 "border-radius: 30px;\n"
-"\n"
-"QMessageBox {\n"
-"    color: rgb(11, 68, 77);\n"
-"    font-size: 14px;\n"
-"}\n"
 "")
         self.groupBox = QGroupBox(loginDialog)
         self.groupBox.setObjectName(u"groupBox")
         self.groupBox.setGeometry(QRect(0, 0, 420, 350))
         self.groupBox.setMinimumSize(QSize(420, 350))
         self.groupBox.setMaximumSize(QSize(420, 350))
+        palette1 = QPalette()
+        palette1.setBrush(QPalette.Active, QPalette.Button, brush1)
+        palette1.setBrush(QPalette.Active, QPalette.Base, brush1)
+        palette1.setBrush(QPalette.Active, QPalette.Window, brush1)
+        palette1.setBrush(QPalette.Inactive, QPalette.Button, brush1)
+        palette1.setBrush(QPalette.Inactive, QPalette.Base, brush1)
+        palette1.setBrush(QPalette.Inactive, QPalette.Window, brush1)
+        palette1.setBrush(QPalette.Disabled, QPalette.Button, brush1)
+        palette1.setBrush(QPalette.Disabled, QPalette.Base, brush1)
+        palette1.setBrush(QPalette.Disabled, QPalette.Window, brush1)
+        self.groupBox.setPalette(palette1)
         self.log_passwordField = QLineEdit(self.groupBox)
         self.log_passwordField.setObjectName(u"log_passwordField")
         self.log_passwordField.setGeometry(QRect(30, 160, 341, 61))
-        font = QFont()
-        font.setFamilies([u"Baloo Chettan 2"])
-        font.setPointSize(16)
-        self.log_passwordField.setFont(font)
+        font1 = QFont()
+        font1.setFamilies([u"Baloo Chettan 2"])
+        font1.setPointSize(16)
+        self.log_passwordField.setFont(font1)
         self.log_passwordField.setStyleSheet(u"background-color: rgb(255, 255, 255);\n"
 "border-radius: 20px;\n"
 "border-width: 10px;\n"
@@ -52,10 +82,12 @@ class Ui_loginDialog(object):
 "border-color: rgb(0, 207, 230);\n"
 "padding: 12px;\n"
 "color:rgb(10, 24, 25);")
+        self.log_passwordField.setInputMethodHints(Qt.InputMethodHint.ImhNoAutoUppercase|Qt.InputMethodHint.ImhNoPredictiveText|Qt.InputMethodHint.ImhSensitiveData)
+        self.log_passwordField.setEchoMode(QLineEdit.EchoMode.PasswordEchoOnEdit)
         self.log_usernameField = QLineEdit(self.groupBox)
         self.log_usernameField.setObjectName(u"log_usernameField")
         self.log_usernameField.setGeometry(QRect(30, 90, 341, 61))
-        self.log_usernameField.setFont(font)
+        self.log_usernameField.setFont(font1)
         self.log_usernameField.setStyleSheet(u"background-color: rgb(255, 255, 255);\n"
 "border-radius: 20px;\n"
 "border-width: 10px;\n"
@@ -66,20 +98,20 @@ class Ui_loginDialog(object):
         self.label = QLabel(self.groupBox)
         self.label.setObjectName(u"label")
         self.label.setGeometry(QRect(40, 50, 81, 21))
-        font1 = QFont()
-        font1.setFamilies([u"Baloo Chettan 2"])
-        font1.setPointSize(20)
-        font1.setBold(True)
-        self.label.setFont(font1)
+        font2 = QFont()
+        font2.setFamilies([u"Baloo Chettan 2"])
+        font2.setPointSize(20)
+        font2.setBold(True)
+        self.label.setFont(font2)
         self.label.setStyleSheet(u"color: #00cbbc")
         self.log_cancel = QPushButton(self.groupBox)
         self.log_cancel.setObjectName(u"log_cancel")
         self.log_cancel.setGeometry(QRect(80, 260, 111, 41))
-        font2 = QFont()
-        font2.setFamilies([u"Baloo Chettan 2"])
-        font2.setPointSize(18)
-        font2.setBold(True)
-        self.log_cancel.setFont(font2)
+        font3 = QFont()
+        font3.setFamilies([u"Baloo Chettan 2"])
+        font3.setPointSize(18)
+        font3.setBold(True)
+        self.log_cancel.setFont(font3)
         self.log_cancel.setStyleSheet(u"background-color: #00dacc;\n"
 "color: rgb(255, 255, 255);\n"
 "border-radius: 15px;\n"
@@ -88,7 +120,7 @@ class Ui_loginDialog(object):
         self.log_ok = QPushButton(self.groupBox)
         self.log_ok.setObjectName(u"log_ok")
         self.log_ok.setGeometry(QRect(210, 260, 111, 41))
-        self.log_ok.setFont(font2)
+        self.log_ok.setFont(font3)
         self.log_ok.setStyleSheet(u"background-color: #00dacc;\n"
 "color: rgb(255, 255, 255);\n"
 "border-radius: 15px;\n"
@@ -105,7 +137,7 @@ class Ui_loginDialog(object):
     # setupUi
 
     def retranslateUi(self, loginDialog):
-        loginDialog.setWindowTitle(QCoreApplication.translate("loginDialog", u"Dialog", None))
+        loginDialog.setWindowTitle(QCoreApplication.translate("loginDialog", u"Login", None))
         self.groupBox.setTitle("")
         self.log_passwordField.setPlaceholderText(QCoreApplication.translate("loginDialog", u"Password", None))
         self.log_usernameField.setPlaceholderText(QCoreApplication.translate("loginDialog", u"Username", None))
